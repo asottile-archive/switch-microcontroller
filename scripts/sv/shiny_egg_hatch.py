5a965b15fd8499ed4ee1bec9f8a48124f42cc563
Daniel Sottile <danielsottile@gmail.com>
2023-01-04 19:13:07 -0500

    feat: finshed script

--- scripts/sv/shiny_egg_hatch.py
+++ scripts/sv/shiny_egg_hatch.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
 import argparse
+import functools
+import os.path
 import time
 
 import cv2
+import numpy
 import serial
 
 from scripts.engine import all_match
 from scripts.engine import always_matches
 from scripts.engine import Color
 from scripts.engine import do
+from scripts.engine import getframe
 from scripts.engine import match_px
 from scripts.engine import match_text
 from scripts.engine import Point
@@ -20,7 +24,8 @@ from scripts.engine import SERIAL_DEFAULT
 from scripts.engine import Wait
 
 # Initial State: 
-# 1) Have the masuda parents you want in your party (2)
+# IMPORTANT: Plese read all steps here to insure the script functions normally:
+# 1) Have the parents you want in your party (you may have up to 6)
 # 2) Make sure you have all sandwich recipes and that you have enough ingredients
 # for sandwich 25.
 # 3) Have your Flame Body / Magma Armor Pokemon in slot 1 of the box before the
@@ -29,12 +34,15 @@ from scripts.engine import Wait
 # 5) Have nicknames off and auto send to boxes
 # 6) Save in front of Area Zero Gate (the rocky section outside)
 # 7) Progress up to overworld.
-# 8) Start script, including connecting to controller
+# 8) IMPORTANT: to detect shinies, set the background color of boxes that will
+# recieve shinies to BACKGROUND 15, or change the images in shiny_check_images.
+# 9) Start script, including connecting to controller
 
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('--serial', default=SERIAL_DEFAULT)
     parser.add_argument('--boxes', type=int, required=True)
+    parser.add_argument('--silent', type=bool, default=False)
     args = parser.parse_args()
 
     require_tesseract()
@@ -44,9 +52,10 @@ def main() -> int:
     vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
     start_time = 0.0
-    shiny_timer = 0.0
     shiny = False
+    alarm = False
     box = 0
+    check_box = 0
     column = 0
     egg_count = 0
     eggs = 5
@@ -54,10 +63,6 @@ def main() -> int:
     def set_start(vid: object, ser: object) -> None:
         nonlocal start_time
         start_time = time.monotonic()
-
-    def set_shiny_start(vid: object, ser: object) -> None:
-        nonlocal shiny_timer
-        shiny_timer = time.monotonic()
 
     def increment_egg_count(vid: object, ser: object) -> None:
         nonlocal egg_count
@@ -69,17 +74,14 @@ def main() -> int:
         egg_count = 0
 
     def restart_eggs(frame: object) -> bool:
-        return time.monotonic() > start_time + 30 * 60       
+        return time.monotonic() > start_time + 30 * 60 
+     
 
-    def is_shiny_detected() -> bool:
-        shiny_time = time.monotonic()
-        print(f'DEBUG: timing is at {shiny_time - shiny_timer} seconds')
-        return shiny_time >= shiny_timer + 13.75
-
-    def set_shiny(vid: cv2.VideoCapture, ser: serial.Serial) -> None:
-        nonlocal shiny
-        shiny = is_shiny_detected()
-        if shiny: 
+    def set_shiny() -> None:
+        nonlocal shiny, alarm
+        if not shiny:
+            shiny = True
+        if shiny and not args.silent and not alarm: 
             # alarm
             do(Press('!'),
             Wait(1),
@@ -94,12 +96,19 @@ def main() -> int:
             Press('.'),
             Wait(.5),
             )(vid, ser)
+            alarm = True
             print('DEBUG: *****SHINY DETECTED!*****')
 
     def are_we_done(frame: object) -> bool:
         return egg_count >= args.boxes * 30
 
     def bye(vid: object, ser: object) -> None:
+        do(# save the game, exit
+            Press('B'), Wait(2),
+            Press('R'), Wait(2),
+            Press('A'), Wait(3),
+            Press('A'), Wait(1),
+            Press('H'))(vid, ser)
         raise SystemExit
 
     def eggs_done(frame: object) -> bool:
@@ -140,10 +149,7 @@ def main() -> int:
             column += 1
             do(Press('d'), Wait(.5))(vid, ser)
 
-        do(
-            Press('-'), Wait(.5), *((Press('s'), Wait(.4)) * 4),
-            Press('A'), Wait(.5),
-        )(vid, ser)
+        select(vid, ser)
 
         for _ in range(column + 1):
             do(Press('a'), Wait(.4))(vid, ser)
@@ -151,7 +157,76 @@ def main() -> int:
         do(Press('s'), Wait(.4), Press('A'), Wait(.5))(vid, ser)
 
     def hatched_all_eggs(frame: object) -> bool:
-        return box == args.boxes - 1 and column == 5 and eggs == 0 and not shiny
+        return box == args.boxes - 1 and column == 5 and eggs == 0
+
+    def reset_vars(vid: object, ser: object) -> None:
+        nonlocal box, check_box, column, eggs, egg_count
+        box = 0
+        check_box = 0
+        column = 0
+        egg_count = 0
+        eggs = 5
+        print('DEBUG: REACHED RESTART')
+
+    def _extract_shiny_icon(
+        im: numpy.ndarray,
+        dims: tuple[int, int, int],
+        ) -> numpy.ndarray:
+            im = cv2.resize(im, (dims[1], dims[0]))
+
+            top_left = Point(y=60, x=1112).norm(dims)
+            bottom_right = Point(y=92, x=1163).norm(dims)
+            crop = im[top_left.y:bottom_right.y, top_left.x:bottom_right.x]
+
+            color = numpy.array([71, 51, 39])
+            t = numpy.array([1, 1, 1])
+            return cv2.inRange(crop, color - t * 20, color + t * 20)
+
+    @functools.lru_cache
+    def _get_shiny_images(
+            dims: tuple[int, int, int],
+    ) -> tuple[tuple[str, numpy.ndarray], ...]:
+        types_dir = os.path.join(os.path.dirname(__file__), 'shiny_check_images')
+
+        return tuple(
+            (tp, _extract_shiny_icon(cv2.imread(os.path.join(types_dir, tp)), dims))
+            for tp in os.listdir(types_dir)
+        )
+
+    def check_shiny(vid: cv2.VideoCapture, ser: serial.Serial) -> None:
+        nonlocal check_box
+
+        def _detect_shiny() -> None:
+            frame = getframe(vid)
+
+            shiny_images = _get_shiny_images(frame.shape)
+
+            sh_im = _extract_shiny_icon(frame, frame.shape)
+            _, sh = max(((im == sh_im).mean(), fname) for fname, im in shiny_images)
+            print(f'{sh}')
+            if sh == 'shiny.png':
+                set_shiny()
+
+        for direction in 'dadad':
+            _detect_shiny()
+            for _ in range(5):
+                do(Press(direction), Wait(1))(vid, ser)
+                _detect_shiny()
+            do(Press('s'), Wait(.75))(vid, ser)
+
+        for _ in range(2):
+            do(Press('s'), Wait(.75))(vid, ser)
+        for _ in range(5):
+            do(Press('a'), Wait(.75))(vid, ser)
+
+        do(Press('R'), Wait(.75))(vid, ser)
+        check_box += 1
+
+    def check_done(frame: object) -> bool:
+        return check_box == args.boxes
+
+    def check_done_w_shiny(frame: object) -> bool:
+        return shiny and (check_box == args.boxes)
 
     reorient = do(
         Wait(1),
@@ -167,6 +242,11 @@ def main() -> int:
         Press('a'), Wait(.5),
         Press('w'), Wait(.5),
         Press('s'), Wait(.5), 
+    )
+
+    select = do(
+        Press('-'), Wait(.5), Press('s', duration=.8), Wait(.4),
+        Press('A'), Wait(.5),
     )
 
     states = {
@@ -266,9 +346,10 @@ def main() -> int:
                 'MASH_A',
             ),
             (
+                # if it fails, go back to the beginning
                 always_matches,
-                do(Press('!'), Wait(.5), Press('.')),
-                'INVALID',
+                do(Press('B'), Wait(2), Press('Y'), Wait(.5), Press('A'), Wait(10),),
+                'INITIAL',
             ),
         ),
         'MASH_A': (
@@ -309,7 +390,7 @@ def main() -> int:
             (
                 # if the timer runs out, restart the egg grabbing sequence
                 restart_eggs,
-                do(Press('Y'), Wait(.5), Press('A'), Wait(5)),
+                do(Press('Y'), Wait(.5), Press('A'), Wait(10)),
                 'INITIAL',
             ),
             (always_matches, do(Wait(30), Press('A'), Wait(.5)), 'MASH_A'),
@@ -327,10 +408,12 @@ def main() -> int:
                     Press('A'), Wait(.5), Press('A'), Wait(.5),
                     # switch first mon
                     Press('a'), Wait(.5), Press('A'), Wait(.5),
+                    Press('s'), Wait(.5),
                     # move second mon
-                    Press('s'), Wait(.5), Press('A'), Wait(.5), Press('A'), Wait(.5), Press('d'), Wait(.5), Press('A'), Wait(.5),
+                    select,
+                    Press('d'), Wait(.5), Press('d'), Wait(.5), Press('w'), Wait(.5), Press('A'), Wait(.5),
                     # re-orient, exit menu
-                    Press('w'), Wait(.5), Press('R'), Wait(.5), Press('B'), Wait(3), Press('B'), Wait(3),
+                    Press('a'), Wait(.5), Press('R'), Wait(.5), Press('B'), Wait(3), Press('B'), Wait(3),
                 ),
                 'INITIAL_HATCH',
             ),
@@ -374,11 +457,10 @@ def main() -> int:
             (
                 match_px(Point(y=234, x=1151), Color(b=0, g=204, r=255)),
                 do(
-                    # press A on boxes menu
+                   # press A on boxes menu
                     Wait(1), Press('A'), Wait(3),
                     # select first column
-                    Press('-'), Wait(.5), *((Press('s'), Wait(.4)) * 4),
-                    Press('A'), Wait(.5),
+                    select,
                     # move it over
                     Press('a'), Wait(.4), Press('s'), Wait(.4),
                     Press('A'), Wait(.5),
@@ -401,7 +483,7 @@ def main() -> int:
                         invert=True,
                     ),
                 ),
-                do(Press('A'), set_shiny_start, Wait(10)),
+                do(Press('A'), Wait(8)),
                 'HATCH_1',
             ),
             (eggs_done, clear_left, 'NEXT_COLUMN'),
@@ -414,30 +496,41 @@ def main() -> int:
                     match_px(Point(y=624, x=370), Color(b=49, g=43, r=30)),
                     match_px(Point(y=523, x=330), Color(b=37, g=202, r=241)),
                 ),
-                do(set_shiny, Wait(1), Press('A'), Wait(5), egg_hatched),
+                do(Wait(1), Press('A'), Wait(5), egg_hatched),
                 'HATCH_5',
             ),
         ),
         'NEXT_COLUMN': (
-            # (lambda : shiny, do(), 'RESTART_SEQUENCE'),
-            (hatched_all_eggs, bye, 'INITIAL_HATCH'),
+            (hatched_all_eggs, do(
+                # we need to move the last eggs into the column for checking
+                # open menu, into boxes
+                Wait(1), Press('X'), Wait(2), Press('A'), Wait(3),
+                # select party to put it back
+                Press('a'), Wait(.5), Press('s'), Wait(.5),
+                select,
+                # position in first column of box
+                Press('d'), Wait(.5), Press('w'), Wait(.5),
+                # put the hatched ones back
+                move_to_column, Press('A'), Wait(.5),
+                # out to main menu
+                Press('B'), Wait(3),
+            ), 'CHECK_SHINY_MENU'),
             (
                 always_matches,
                 do(
                     # open menu, into boxes
-                    Press('X'), Wait(2), Press('A'), Wait(3),
+                    Wait(1), Press('X'), Wait(2), Press('A'), Wait(3),
                     # select party to put it back
                     Press('a'), Wait(.5), Press('s'), Wait(.5),
-                    Press('-'), Wait(.5), *((Press('s'), Wait(.4)) * 4),
-                    Press('A'), Wait(.5),
+                    select,
                     # position in first column of box
                     Press('d'), Wait(.5), Press('w'), Wait(.5),
                     # put the hatched ones back and pick up new column
                     move_to_column, Press('A'), Wait(.5),
                     pick_up_new_column,
                     # out to main menu
-                    Press('B'), Wait(2),
-                    Press('B'), Wait(1),
+                    Press('B'), Wait(3),
+                    Press('B'), Wait(3),
                     # reorient for next batch
                     Press('Y'), Wait(5), tap_w, tap_s, Wait(.5), Press('l'), Wait(.5)
                 ),
@@ -461,11 +554,31 @@ def main() -> int:
                 'HATCH_5',
             ),
         ),
+        'CHECK_SHINY_MENU': (
+            (
+                match_px(Point(y=241, x=1161), Color(b=0, g=204, r=255)),
+                # press A on boxes menu
+                do(Wait(1), Press('A'), Wait(3), 
+                # action to make sure wallpaper shows up
+                Press('A'), Wait(1), Press('A'), Wait(1), Press('A'), Wait(1)),
+                'CHECK_SHINY',
+            ),
+            (always_matches, do(Press('s'), Wait(.75)), 'CHECK_SHINY_MENU'),
+        ),
+        'CHECK_SHINY': (
+            (check_done_w_shiny, bye, 'EXIT'),
+            (check_done, reset_vars, 'RESET_SEQUENCE'),
+            (always_matches, check_shiny, 'RESET_TIMEOUT'),
+        ),
+        'RESET_TIMEOUT': (
+            (always_matches, do(Wait(1)), 'CHECK_SHINY'),
+        ),
         'RESET_SEQUENCE': (
             (
-                lambda : not shiny, 
+                # only restart the game if no shiny
+                lambda frame: not shiny, 
                 do(
-                    # hard reset the game if no shines
+                    # hard reset the game via home
                     Press('H'), Wait(1),
                     Press('X'), Wait(.5),
                     Press('A'), Wait(4),
@@ -474,8 +587,10 @@ def main() -> int:
                     Press('A'), Wait(20),
                     Press('A'), Wait(22)
                 ),
-                'INITIAL_HATCH'
-            )
+                'INITIAL'
+            ),
+            # catch all in case for some god forsaken reason it enters this stage with a shiny detected
+            (always_matches, bye, 'INVALID')
         )
     }
 
